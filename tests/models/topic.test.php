25cbbbcc7f01a66f1d9e0b84fbacc6f111a2993e
crynobone <crynobone@gmail.com>
2013-01-27 01:14:21 +0800

    update Diskus\Model\Topic test case

--- tests/models/topic.test.php
+++ tests/models/topic.test.php
@@ -12,6 +12,9 @@ class ModelsTopicTest extends Diskus\Testable\TestCase {
 	 */
 	public function testConstantIsProperlyDefined()
 	{
+		$this->assertEquals(1, Diskus\Model\Topic::ANSWERED);
+		$this->assertEquals(0, Diskus\Model\Topic::NOT_ANSWERED);
+
 		$this->assertEquals('draft', Diskus\Model\Topic::STATUS_DRAFT);
 		$this->assertEquals('private', Diskus\Model\Topic::STATUS_PRIVATE);
 		$this->assertEquals('deleted', Diskus\Model\Topic::STATUS_DELETED);
