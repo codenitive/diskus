a6eff0531659d46b8349c26a32ee3c34b49eb544
crynobone <crynobone@gmail.com>
2013-01-27 10:01:25 +0800

    Draft tags controller.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ controllers/api/tags.php
@@ -0,0 +1,18 @@
+<?php
+
+use Orchestra\View;
+
+class Diskus_Api_Tags_Controller extends Controller {
+
+	/**
+	 * Use restful verb.
+	 *
+	 * @var boolean
+	 */
+	public $restful = true;
+
+	public function get_index()
+	{
+		
+	}
+}
