9a610fb944ddfc9032174249ea71026397470671
crynobone <crynobone@gmail.com>
2013-03-10 14:57:02 +0800

    Simplify the views.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- views/api/topics/edit.blade.php
+++ views/api/topics/edit.blade.php
@@ -1,13 +1,3 @@
 @include(locate('diskus::api.widgets.menu'))
-
-<div class="page-header">
-	
-	<h2>{{ isset($_title_) ? $_title_ : 'Diskus' }}
-		@if ( ! empty($_description_))
-		<small>{{ $_description_ ?: '' }}</small>
-		@endif
-	</h2>
-
-</div>
-
+@include(locate('orchestra::layout.widgets.header'))
 {{ $form }}
--- views/api/topics/index.blade.php
+++ views/api/topics/index.blade.php
@@ -1,19 +1,4 @@
 @include(locate('diskus::api.widgets.menu'))
-
-<div class="page-header">
-	
-	<div class="pull-right">
-		<a href="{{ URL::current() }}/view" class="btn btn-primary">
-			{{ __('diskus::title.topics.create') }}
-		</a>
-	</div>
-	
-	<h2>{{ isset($_title_) ? $_title_ : 'Diskus' }}
-		@if ( ! empty($_description_))
-		<small>{{ $_description_ ?: '' }}</small>
-		@endif
-	</h2>
-
-</div>
-
+<?php Orchestra\Site::set('header::add-button', true); ?>
+@include(locate('orchestra::layout.widgets.header'))
 {{ $table }}
